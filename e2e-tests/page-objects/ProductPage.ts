bedb7699efd4f4bf68b5bd36a94bb536879033c9
Kstala <krzysiek.stala@gmail.com>
2024-04-22 23:26:53 +0200

    chore: select variant fixes

--- e2e-tests/page-objects/ProductPage.ts
+++ e2e-tests/page-objects/ProductPage.ts
@@ -2,21 +2,13 @@ import { expect, Locator, Page } from '@playwright/test';
 
 export class ProductPage {
   readonly page: Page;
-  readonly addToCartButton: Locator;
-  readonly variant: Locator;
-  readonly variantText: Locator;
-  readonly productOption: Locator;
-  readonly miniCartLink: Locator;
-  readonly productRemove: Locator;
+  readonly sizeLocator: Locator;
+  readonly colourLocator: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.addToCartButton = page.getByTestId('add-to-cart-button');
-    this.variant = page.getByTestId('product-variant');
-    this.variantText = page.getByTestId('product-variant-text');
-    this.productOption = page.getByTestId('cart-product-options');
-    this.miniCartLink = page.getByTestId('cart-button');
-    this.productRemove = page.getByTestId('product-remove-button');
+    this.sizeLocator = page.getByRole('button', { name: 'M' });
+    this.colourLocator = page.getByRole('button', { name: 'blue' });
   }
 
   async addToCart() {
@@ -32,10 +24,16 @@ export class ProductPage {
   }
 
   async selectVariant() {
-    await this.page.waitForSelector("button[title='Size M']");
-    await this.page.waitForLoadState('domcontentloaded');
-    await this.page.getByRole('button', { name: 'blue' }).click();
-    await this.page.waitForLoadState('networkidle');
-    await this.page.getByRole('button', { name: 'M' }).click();
+    await expect(async () => {
+      await this.page.waitForSelector("button[title='Size M']");
+      await this.sizeLocator.dispatchEvent('click');
+      await this.page.waitForSelector("button[title='Colour blue']");
+      await this.colourLocator.dispatchEvent('click');
+      await expect(this.sizeLocator).toHaveClass(/ring-2/);
+    }).toPass({
+      // Probe, wait 1s, probe, wait 2s, probe, wait 10s, probe, wait 10s, probe, .... Defaults to [100, 250, 500, 1000].
+      intervals: [1_000, 2_000, 10_000],
+      timeout: 60_000
+    });
   }
 }
