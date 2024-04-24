5b5bd3999ced22400af612f743e20d1ba2020445
Björn Meyer <b.meyer@shopware.com>
2024-04-24 09:14:15 +0200

    fix prettier style

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -4,7 +4,7 @@ import { sorting } from 'lib/constants';
 
 export default function Loading() {
   return (
-    <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black dark:text-white md:flex-row">
+    <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row dark:text-white">
       <div className="order-first w-full flex-none md:max-w-[125px]"></div>
       <div className="order-last min-h-screen w-full md:order-none">
         <Grid className="grid-cols-2 lg:grid-cols-3">
