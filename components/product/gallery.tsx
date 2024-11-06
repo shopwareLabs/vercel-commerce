4274192617ba61f1313b0d468669b6c086db0210
mkucmus <maciejkucmus@gmail.com>
2024-11-06 22:23:46 +0100

    fix: product gallery

--- components/product/gallery.tsx
+++ components/product/gallery.tsx
@@ -4,6 +4,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import { GridTileImage } from 'components/grid/tile';
 import { useProduct, useUpdateURL } from 'components/product/product-context';
 import Image from 'next/image';
+import { startTransition } from 'react';
 
 export function Gallery({ images }: { images: { src: string; altText: string }[] }) {
   const { state, updateImage } = useProduct();
@@ -12,6 +13,13 @@ export function Gallery({ images }: { images: { src: string; altText: string }[]
 
   const nextImageIndex = imageIndex + 1 < images.length ? imageIndex + 1 : 0;
   const previousImageIndex = imageIndex === 0 ? images.length - 1 : imageIndex - 1;
+
+  const formAction = (img: string) => {
+    startTransition(async () => {
+      const newState = updateImage(img);
+      updateURL(newState);
+    });
+  };
 
   const buttonClassName =
     'h-full px-6 transition-all ease-in-out hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center';
@@ -34,9 +42,8 @@ export function Gallery({ images }: { images: { src: string; altText: string }[]
           <div className="absolute bottom-[15%] flex w-full justify-center">
             <div className="mx-auto flex h-11 items-center rounded-full border border-white bg-neutral-50/80 text-neutral-500 backdrop-blur dark:border-black dark:bg-neutral-900/80">
               <button
-                formAction={() => {
-                  const newState = updateImage(previousImageIndex.toString());
-                  updateURL(newState);
+                onClick={() => {
+                  formAction(previousImageIndex.toString());
                 }}
                 aria-label="Previous product image"
                 className={buttonClassName}
@@ -45,9 +52,8 @@ export function Gallery({ images }: { images: { src: string; altText: string }[]
               </button>
               <div className="mx-1 h-6 w-px bg-neutral-500"></div>
               <button
-                formAction={() => {
-                  const newState = updateImage(nextImageIndex.toString());
-                  updateURL(newState);
+                onClick={() => {
+                  formAction(nextImageIndex.toString());
                 }}
                 aria-label="Next product image"
                 className={buttonClassName}
@@ -58,7 +64,6 @@ export function Gallery({ images }: { images: { src: string; altText: string }[]
           </div>
         ) : null}
       </div>
-
       {images.length > 1 ? (
         <ul className="my-12 flex items-center justify-center gap-2 overflow-auto py-1 lg:mb-0">
           {images.map((image, index) => {
@@ -67,9 +72,8 @@ export function Gallery({ images }: { images: { src: string; altText: string }[]
             return (
               <li key={image.src} className="h-20 w-20">
                 <button
-                  formAction={() => {
-                    const newState = updateImage(index.toString());
-                    updateURL(newState);
+                  onClick={() => {
+                    formAction(index.toString());
                   }}
                   aria-label="Select product image"
                   className="h-full w-full"
